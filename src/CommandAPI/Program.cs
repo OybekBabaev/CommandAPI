e5170f709e40912e8b4840aea297888e2497d582
Oybek Babaev <oybekbabaev15@yahoo.com>
2022-07-15 12:25:32 +0500

    Set User Secrets and added dev-env connection string

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,15 +1,23 @@
 using CommandAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionStringBuilder = new NpgsqlConnectionStringBuilder();
+connectionStringBuilder.ConnectionString = 
+    builder.Configuration.GetConnectionString("PostgreSqlConnection");
+connectionStringBuilder.Username = builder.Configuration["UserID"];
+connectionStringBuilder.Password = builder.Configuration["Password"];
+
 builder.Services.AddDbContext<CommandContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
+    options.UseNpgsql(connectionStringBuilder.ConnectionString));
 builder.Services.AddControllers();
 builder.Services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment()) app.UseDeveloperExceptionPage();
 app.MapGet("/", async ctx => await ctx.Response.WriteAsync("Hallo Welt!"));
 app.MapControllers();
 
